75fce9963cb4a74ea749c81f0880e5ba4787daf4
liminal18 <aljones15@gmail.com>
2016-10-29 13:09:49 +0700

    redux connect not working

---
+++ business/actions.js
@@ -0,0 +1,7 @@
+export const UPDATE_RSS = "UPDATE_RSS";
+export const UPDATE_ITEMS = "UPDATE_ITEMS";
+export const UPDATE_ITEMS_FAILED = "UPDATE_ITEMS_FAILED";
+export const UPDATING_ITEMS = "UPDATING_ITEMS";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+export const SHOW_ITEM = "SHOW_ITEM";
+export const REMOVE_ERROR = "REMOVE_ERROR";
--- index.ios.js
+++ index.ios.js
@@ -15,33 +15,14 @@ import {
   WebView
 } from 'react-native';
 
-import Redux, { createStore } from 'redux';
-import { connect } from 'react-redux';
 import { Button, Subheader } from 'react-native-material-design';
-
-var initialState = {};
-
-const UPDATE_ITEMS = "UPDATE_ITEMS";
-
-function reduceItems(state = initialState, action){
-  if(typeof state === 'undefined') return initialState;
-  switch (action.type) {
-    case UPDATE_ITEMS:
-      return initialState
-    default:
-      return state
-  }
-}
-
-let store = createStore(reduceItems);
+import { store } from './business/reduxCreate.js';
+import { connect, Provider } from 'react-redux';
 
 let yahooQLbase = "https://query.yahooapis.com/v1/public/yql?q=select * from rss where url=";
-// https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20=%20%27http://boingboing.net/feed%27
-
 
 function getRss(url, cb){
     let uri = encodeURI(yahooQLbase + "'" + url + "'");
-    console.log('uri is ' + uri);
     return fetch(uri, {
     method: 'GET',
     headers: {
@@ -81,180 +62,7 @@ class ITEM extends Component {
   }
 }
 
-class COMMENT extends Component {
-  render() {
-    <View>
-      {this.props.children}
-    </View>
-  }
-}
 
-class ENCLOSURE extends Component {
-  render() {
-    <View>
-      {this.props.children}
-    </View>
-  }
-}
-
-class AUTHOR extends Component {
-  render() {
-    <View>
-      {this.props.children}
-    </View>
-  }
-}
-
-class CATEGORY extends Component {
-  render() {
-    <View>
-      {this.props.children}
-    </View>
-  }
-}
-
-
-
-class LINK extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class URL extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class IMAGE extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class WIDTH extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class HEIGHT extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class DESCRIPTION extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class LANGUAGE extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class RATING extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class COPYRIGHT extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class PUBDATE extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class LASTBUILDDATE extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class DOCS extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class CLOUD extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class TTL extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class MANAGINGEDITOR extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class WEBMASTER extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class SKIPHOURS extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-
-class HOUR extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class SKIPDAYS extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
-
-class DAY extends Component {
-  render() {
-    <View>
-    </View>
-  }
-}
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2 });
 
@@ -267,7 +75,6 @@ class test3 extends Component {
     };
   }
   printResult(responseText){
-      console.log(responseText.query.results.item[0]);
       if(responseText.query && responseText.query.results && responseText.query.results.item){
         this.setState({ rss: ds.cloneWithRows(responseText.query.results.item) });
       }
@@ -279,19 +86,40 @@ class test3 extends Component {
     return rss;
   }
   componentDidMount(){
+    console.log("testing props passes");
+    console.log(this.props);
     this.fetchResult();
   }
   render() {
     return(
-      <View>
-      <ListView
-        dataSource={this.state.rss}
-        renderRow={(rowData) => <ITEM item={rowData}>item</ITEM>}
-      />
-      </View>
+      <Provider store={store}>
+       <View>
+       <ListView
+         dataSource={this.state.rss}
+         renderRow={(rowData) => <ITEM item={rowData}></ITEM>}
+       />
+       </View>
+      </Provider>
     );
   }
 }
+
+const mapStateToProps = (state, props) => {
+  console.log("map state to props");
+  return {
+    test: "test",
+    items: state.rss
+  };
+}
+
+const dispatchToStore = (dispatch) => {
+  console.log("map dispatch to props");
+  return {
+    selectItem: () => { console.log("select item"); }
+  }
+}
+
+export default connect(mapStateToProps, dispatchToStore)(test3)
 
 const styles = StyleSheet.create({
   container: {
