b14a750b8a299b5411dc0e0a08340f00f24c74dd
liminal18 <aljones15@gmail.com>
2016-12-12 21:32:39 +0700

    rgba color scheme is in

--- assets/styles.js
+++ assets/styles.js
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native';
 
-const headerBG = '#84C9F5';
+const headerBG = 'rgba(253, 195, 195, 0.78)';
 export const fontColor = '#011827';
-export const evenItemColor = '#EDAFAF';
-export const oddItemColor = '#74FF3D';
+export const evenItemColor = 'rgba(146, 230, 251, 0.78)';
+export const oddItemColor = 'rgba(146, 251, 167, 0.58)';
 
 export const sizes = StyleSheet.create({
   flexOne: {
