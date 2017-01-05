a031bad075049495abdf7be9ddfb4f93eba547ee
liminal18 <aljones15@gmail.com>
2017-01-05 09:43:48 +0700

    just making sure the regex works

--- __tests__/storageAddUrl.js
+++ __tests__/storageAddUrl.js
@@ -1,6 +1,7 @@
 require("babel-polyfill");
 const mockStorage = require('mock-async-storage');
 import * as storage from '../assets/Services/asyncStorage.js';
+import { isUrl } from '../assets/Services/validation.js';
 
 describe("should catch a ", () => {
   mockStorage.mock();
@@ -18,5 +19,10 @@ describe("should catch a ", () => {
     urls = await storage.getAllSubs();
     expect(urls.length).toBe(1);
   })
-
+  it("invalid url", () => {
+    let result = isUrl.test("hftr:/\eee.s.org");
+    expect(result).toBe(false);
+    result = isUrl.test("http://www.world.com");
+    expect(result).toBe(true);
+  })
 })
