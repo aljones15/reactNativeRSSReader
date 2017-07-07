bccdd67c922608c3ec32967763251f01dc8c054a
liminal18 <aljones15@gmail.com>
2017-07-07 22:19:23 +0700

    fixed the paginate tests

--- __tests__/paginate.js
+++ __tests__/paginate.js
@@ -1,6 +1,6 @@
 "use strict";
-import { store } from '../assets/Services/reduxCreate.js';
-import { INCREMENT_SKIP, DECREMENT_SKIP, RESET_SKIP} from '../assets/actions.js';
+import { store } from '../assets/Services/redux/index.js';
+import { INCREMENT_SKIP, DECREMENT_SKIP, RESET_SKIP} from '../assets/Services/redux/actions.js';
 
 describe("Should ", () => {
     
@@ -8,20 +8,20 @@ describe("Should ", () => {
     const skip = 100;
     store.dispatch({type: INCREMENT_SKIP, payload: skip});
     const state = store.getState();
-    expect(state.reduceSkip.skip).toBe(skip);
+    expect(state.skip.skip).toBe(skip);
   })
 
   it("decrement skip from 10 to 0", () => {
     const skip = 0;
     store.dispatch({type: DECREMENT_SKIP, payload: skip});
     const state = store.getState();
-    expect(state.reduceSkip.skip).toBe(skip);
+    expect(state.skip.skip).toBe(skip);
   })
 
   it("reset skip to 0", () => {
     store.dispatch({type: RESET_SKIP});
     const state = store.getState();
-    expect(state.reduceSkip.skip).toBe(0);
+    expect(state.skip.skip).toBe(0);
   })
 /*
   it("should not increment", () => {
